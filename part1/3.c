1e822ae967b14795b7695f58f2384bea267bf1e9
mariak0 <83291320+mariak0@users.noreply.github.com>
2022-04-29 22:49:44 +0300

    Update 3.c

--- part1/3.c
+++ part1/3.c
@@ -4,9 +4,8 @@
 #include <ctype.h>
 #include <time.h>
 
-//creat a struct in order to store csv's data
 typedef struct data {
-char date[10];
+long int date;
 float T_degC;
 float PO4uM;
 float SiO3uM;
@@ -16,20 +15,8 @@ float Salnty;
 float O2ml_L
 } data;
 
-typedef struct data2 {
-long int date[10];
-float T_degC;
-float PO4uM;
-float SiO3uM;
-float NO2uM;
-float NO3uM;
-float Salnty;
-float O2ml_L
-} data2;
-
+//open file ocean.csv
 void openCSV(data data[]){
-    // Substitute the full file path
-    // for the string file_path
         FILE* fp = fopen("ocean.csv", "r");
 
     if (!fp)
@@ -54,13 +41,17 @@ void openCSV(data data[]){
                 char* value = strtok(buffer, ", ");
 
                 while (value) {
-            
+
                     if (column == 0) {
-                        strcpy(data[r].date, value);
+
+
+                        removeChar(value, '/');
+                        swapdate(value);
+                        data[r].date = atol(value);
                     }
 
                 if (column == 1) {data[r].T_degC = atof(value);}
-                    
+
                 if (column == 2) {data[r].PO4uM = atof(value);}
 
                 if (column == 3) {data[r].SiO3uM = atof(value);}
@@ -85,245 +76,237 @@ void openCSV(data data[]){
     }
 }
 
-void openCSV2(data2 data[]){
-    FILE* fp = fopen("new.csv", "r");
-
-    if (!fp)
-        printf("Can't open file\n");
-
-    else {
-
-        char buffer[1024];
-
-
-
-        int row = 0;
-        int column = 0;
-        int r=0;
-
-        while (fgets(buffer,1024, fp)) {
-            column = 0;
-            row++;
-            r++;
-
-
-
-            if (row == 1)
-                continue;
-
-			char* value = strtok(buffer, ", ");
-
-            while (value) {
-                    
-                if (column == 0) {
-                    data[r].date = atol(value);
-                }
-           
-
-                if (column == 1) {
-                    data[r].T_degC = atof(value);
-                }
-
-
-                if (column == 2) {
-                  data[r].PO4uM= atof(value);  //we save the phosphate as long integers
-                }
-                if (column == 3) {
-                    data[r].SiO3uM = atof(value);
-                }
-                if (column == 4) {
-                    data[r].NO2uM = atof(value);
-                }
-                if (column == 5) {
-                    data[r].NO3uM = atof(value);
-                }
-                if (column == 6) {
-                    data[r].Salnty = atof(value);
-                }
-
-                if (column == 7) {
-                    data[r].O2ml_L = atof(value);
-                }
-
-                value = strtok(NULL, ", ");
-                column++;
-            }
-
-            printf("\n");
-        }
-
-        // Close the file
-        fclose(fp);
-}
-}
-
-void createCSV2(data2 data[],int s){
+//create a new csv file to save date as long int
+void createCSV2(data data[],int s){
      FILE *fpt;
+
 	int i;
-    fpt = fopen("new.csv", "w+");
+
+	fpt = fopen("new.csv", "w+");
     fprintf(fpt,"Date,       PO4Um,       T_degC,      SiO3uM,      NO2uM,      NO3uM,      Salnty,       O2ml_L\n");
-    
+
     for (i=2; i<s; i++)
     {
-		
-        fprintf(fpt,"%s,  %f,   %f,   %f,   %f,   %f,   %f,   %f\n",data[i].date,  data[i].T_degC, data[i].PO4uM,data[i].SiO3uM,data[i].NO2uM, data[i].NO3uM,data[i].Salnty, data[i].O2ml_L);
+
+        fprintf(fpt,"%ld,  %f,   %f,   %f,   %f,   %f,   %f,   %f\n",data[i].date,  data[i].T_degC, data[i].PO4uM,data[i].SiO3uM,data[i].NO2uM, data[i].NO3uM,data[i].Salnty, data[i].O2ml_L);
     }
 
     fclose(fpt);
  }
 
-void keep_number(data2 data[], char str[]){
-	
-	int i=0;
-    int j = 0;
-	
-    for (i=2; str[i] ; i++)
-    {
-        
-        if (str[i] >= '0' && str[i] <= '9')
-        {
-            str[j] = str[i];
-            j++;
-        }
+
+//function to remove th "/" from the dates
+void removeChar(char* s, char c)
+{
+
+    int j, n = strlen(s);
+    for (int i = j = 0; i < n; i++)
+        if (s[i] != c)
+            s[j++] = s[i];
+
+    s[j] = '\0';
+}
+
+
+//function to swap the date as YYYY/MM/DD to be in ascending order
+void swapdate (char* str)
+{
+    char temp;
+    int n;
+
+    n = strlen(str);
+	// swap str[0] and str[n-4]
+	temp = str[0];
+	str[0] = str[n - 4];
+	str[n - 4] = temp;
+	// swap str[1] and str[n-3]
+	temp = str[1];
+	str[1] = str[n - 3];
+	str[n - 3] = temp;
+	// swap str[2] and str[n-2]
+	temp = str[2];
+	str[2] = str[n - 2];
+	str[n - 2] = temp;
+	// swap str[3] and str[n-1]
+	temp = str[3];
+	str[3] = str[n - 1];
+	str[n - 1] = temp;
+
+}
+
+
+//binary search
+int binarySearch(data data[], int left, int right, long int w){
+
+    while (left <= right) {
+	    int m = left + (right - left) / 2;
+
+	    if (data[m].date == w)
+	      return m;
+
+	    if (data[m].date < w)
+	      left = m + 1;
+
+	    else
+	      right = m - 1;
+  }
+
+  return -1;
+}
+
+//interpolation search
+int interpolationSearch(data data[], int l, int h, long int w){
+
+    int pos;
+
+    if (l <= h && w >= data[l].date && data[h].date){
+
+       pos = l + (((double)(h - l) / (data[h].date - data[l].date))
+                 * (w - data[l].date));
+
+        //target found
+        if (data[pos].date == w)
+            return pos;
+
+        //w larger, w in right area
+        if (data[pos].date < w)
+            return interpolationSearch(data, pos + 1, h, w);
+
+        //w smaller, w in left area
+        if (data[pos].date > w)
+            return interpolationSearch(data, l, pos - 1, w);
     }
-
-    str[i] = '\0';
-
-    
-}
-
-int binarySearch(data data[], int Left, int Right, char W){
-
-    if(Right>=1){
-
-        int m=(Left+(Right-1))/2; //find the middle of the array
-
-
-            if(data[m].date==W) //W(anted) element is in the middle
-            	return m;
-
-            if(data[m].date>W) //W(anted) element is smaller than mid
-                return binarySearch(data, Left, m-1, W); //smaller list, change Right
-
-            //W is after the middle (to the Right)
-            else return binarySearch(data, m+1, Right , W); //smaller list, change Left
-    }
-
-    return -1; //the array has only one element, nothing to search
-
+    return -1;
 }
 
 
-/*int interpolationSearch(data data[], int n, char W){     //array of sorted data, n=number, W=value we want
-    if(n==0)
-        { printf("Error! No data found");
-          exit(1);}
+void swap(int *a, int *b) {
+  int t = *a;
+  *a = *b;
+  *b = t;
+}
 
-    int l=0, h=n-1, mid; //l=lower bound, h=higher bound, mid=middle
 
-    //loop
-    while (data[h].date != data[l].date && W >= data[l].date && W <= data[h].date)
-        {
-            //find mid
-            mid = l + ((W - data[l].date) * (h - l) / (data[h].date - data[l].date));
+//shorting by date
+void insertionSort(data arr[], int n)
+{
+    int i,  j;
+    long int key;
+    for (i = 1; i < n; i++) {
+        key = arr[i].date;
+        j = i - 1;
 
-            //value is found
-            if (W == data[mid].date) {return mid;}
-
-            else if(W < data[mid].date) {h = mid - 1;} //change high bound
-
-            else {l = mid + 1;} //change low bound
+        while (j >= 0 && arr[j].date > key) {
+            arr[j + 1].date= arr[j].date;
+            swap(&arr[j + 1].T_degC, &arr[j].T_degC);
+            swap(&arr[j + 1].PO4uM, &arr[j].PO4uM);
+            swap(&arr[j + 1].SiO3uM, &arr[j].SiO3uM);
+            swap(&arr[j + 1].NO2uM, &arr[j].NO2uM);
+            swap(&arr[j + 1].NO3uM, &arr[j].NO3uM);
+            swap(&arr[j + 1].Salnty, &arr[j].Salnty);
+            swap(&arr[j + 1].O2ml_L, &arr[j].O2ml_L);
+            j = j - 1;
         }
-
-
-
-    //value found
-    if(W == data[l].date) {return l;}
-
-    //value doesn't exist
-    else {return -1;}
+        arr[j + 1].date = key;
+    }
 }
-*/
 
+
+//main
 int main() {
 
-  data d[1500];
-  data data[1500];
 
-  openCSV(d); //call the function that read the csv file
-  openCSV2(data);
+		data d[1406];
+
+		openCSV(d); //call the function that read the csv file
+
+		int s=sizeof(d)/sizeof(d[0]); //used to find right limit, binary search
+
+		//shorting struct
+		insertionSort(d,s);
+		createCSV2(d,s);
+
+		char dat[10]; //date
+		char h; //variable to save user's choice
+		int date;
+
+		printf("Enter the date: (MM/DD/YYYY) \n"); //ask for a date
+		scanf("%s", &dat);
+	    removeChar(dat,'/'); //removing / from the date
+	    swapdate(dat);	//change date format from MM/DD/YYYY to YYYY/MM/DD
+	    date = atol(dat);  //make date a long int
 
 
-    int s=sizeof(data)/sizeof(data[0]); //used to find right limit, binary search
-    int n=sizeof(data);
+		printf("Select what do you want to search for:\n"); //ask for temperature, phosphate or both
+		printf("Type t for Temperature \nType p for Phosphate \nType b for both Temperature and Phosphate \n ");
+		scanf("%s", &h);
 
-    int date; //date
-    int h; //variable to save user's choice
+	   clock_t start, end;
+	   //start the clock
+	   start = clock();
 
-  printf("Enter the date:\n"); //ask for a date
-  scanf("%s",&date);
+	printf("\nClock at starting time: %ld\n", start);
 
-  printf("Select what do you want to search for:\n"); //ask for temp, phosp or both
-  printf(" Type t for Temperature \n Type p for Phosphate \n Type b for both Temperature and Phosphate \n ");
-  scanf("%d", &h);
+	int result_b = binarySearch(d, 0 , s-1, date);
+	int result_i = interpolationSearch(d, 0, s-1, date);
 
 
-   int result_temp_bin = binarySearch(data, 0 , s-1, date);
- //  int result_temp_inter = interpolationSearch(data, n, date);
-   int result_phosp_bin = binarySearch(data, 0 , s-1, date);
-  // int result_phosp_inter = interpolationSearch(data, n, date);
-   int result_b_bin = binarySearch(data, 0 , s-1, date);
-  // int result_b_inter = interpolationSearch(data, n, date);
+	//stop the clock
+	end = clock();
 
-    switch(h) //cases for every choice
-    {
-        case 't': //only temperature
+		switch(h) //cases for every choice
+	    {
+	        case 't': //only temperature
 
-	            //binary search for temp
-	            if (result_temp_bin != -1) {printf("Temperature found with binary search: %f \n", data[result_temp_bin].T_degC);}
-	            if (result_temp_bin == -1) {printf("No data found\n");}
+		            //binary search for temp
+		            if (result_b == -1) {printf("No data found\n");}
+		            	else {printf("Temperature, found with binary search, is: %.2f \n", d[result_b].T_degC);}
 
-	            //interpolation search for temp
-	         //   if (result_temp_inter != -1) {printf("Temperature found with interpolation search: %f \n", data[result_temp_inter].T_degC);}
-	        //    if(result_temp_inter == -1) {printf("No data found\n");}
+                    //interpolation search for temp
+                    if (result_i == -1) {printf("No data found\n");}
+		            	else {printf("Temperature, found with interpolation search, is: %.2f \n", d[result_i].T_degC);}
 
-            break;
+	        break;
 
-        case 'p': //only phosphate
 
-	            //binary search for temp
-	            if (result_phosp_bin != -1) {printf("The value of the phosphate found with binary search: %f \n", data[result_phosp_bin].PO4uM);}
-	            if (result_phosp_bin == -1) {printf("No data found\n");}
+	        case 'p': //only phosphate
 
-	            //interpolation search for temp
-	          //  if (result_phosp_inter != -1) {printf("The value of the phosphate found with interpolation search: %f \n", data[result_phosp_inter].PO4uM);}
-	           // if (result_phosp_inter == -1) {printf("No data found\n");}
+		            //binary search for phosp
+		            if (result_b == -1) {printf("No data found\n");}
+		            	else {printf("The value of phosphate, found with binary search, is: %.2f \n", d[result_b].PO4uM);}
 
-            break;
+                    //interpolation search for phosp
+                    if (result_i == -1) {printf("No data found\n");}
+		            	else {printf("The value of phosphate, found with interpolation search, is: %.2f \n", d[result_i].PO4uM);}
+	        break;
 
-        case 'b': //both temperature and phosphate
 
-	            //binary search for both
-	            if (result_b_bin != -1) {printf("The value of the phosphate found with binary search: %f ", data[result_b_bin].PO4uM, "and the temperature was: %f \n", data[result_b_bin].T_degC);}
-	            if (result_b_bin == -1) {printf("No data found\n");}
+	        case 'b': //both temperature and phosphate
 
-	             //interpolation search for temp
-//	            if (result_b_inter != -1) {printf("The value of the phosphate found with binary search: %f ", data[result_b_inter].PO4uM, "and the temperature was: %f \n", data[result_b_inter].T_degC);}
-	//            if (result_b_inter == -1) {printf("No data found\n");}
+		            //binary search for both
+		            if (result_b == -1) {printf("No data found\n");}
+		            	else{
+							printf("The value of phosphate, found with binary search, is: %.2f ", d[result_b].PO4uM);
+		                	printf("and temperature is: %.2f \n", d[result_b].T_degC);
+						}
 
-            break;
+                    //interpolation search for both
+                    if (result_i == -1) {printf("No data found\n");}
+		            	else{
+							printf("The value of phosphate, found with interpolation search, is: %.2f ", d[result_i].PO4uM);
+		                	printf("and temperature is: %.2f \n", d[result_i].T_degC);
+						}
+	        break;
 
-        //doesn't match any case
-        default:
-            printf("Error! This is not a choice!\n");//In case user type wrong letter
-    }
+	        //doesn't match any case
+	        default:
+	            printf("Error! This is not a choice!\n");//In case user type wrong letter
+	    }
 
-    /*char cont_prog = 'n';
-            do {
-                 main program in here
-                printf("Do you want to start again? (y/n): ");
-                cont_prog = getchar();
-            } while (cont_prog == 'y' || cont_prog == 'Y'); */
+        printf("\nClock ticks at end time: %ld\n", end);
+        printf("CLOCKS_PER_SEC: %ld\n", CLOCKS_PER_SEC);
+        printf("The duration in seconds since the program was launched: %fl\n\n", (double)(end-start)/CLOCKS_PER_SEC);
+
 
   return 0;
 }
