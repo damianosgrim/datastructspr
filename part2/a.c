b669500239b83520962004a1833b281089da2ca6
Damianos Grimanis <83086568+damianosgrim@users.noreply.github.com>
2022-05-19 20:49:36 +0300

    Create a.c

---
+++ part2/a.c
@@ -0,0 +1,291 @@
+#include <stdio.h>
+#include <conio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+#include <time.h>
+
+
+typedef struct Data {
+long int date;
+float T_degC;
+float PO4uM;
+float SiO3uM;
+float NO2uM;
+float NO3uM;
+float Salnty;
+float O2ml_L;
+} data;
+
+//Συνάρτηση που ανοίγει το csv και τα αποθηκεύει σε Stucts
+void openCSV(data data[]){
+    FILE* fp = fopen("ocean.csv", "r");
+
+    if (!fp)
+        printf("Can't open file\n");
+
+    else {
+        char buffer[1024];
+        int row = 0;
+        int column = 0;
+        int r=0;
+
+        while (fgets(buffer,1024, fp)) {
+            column = 0;
+            row++;
+            r++;
+            if (row == 1)
+                continue;
+            char* value = strtok(buffer, ", ");
+            while (value) {
+
+                if (column == 0) {
+
+                        removeChar(value, '/');
+                        swapdate(value);
+                        data[r].date = atol(value);
+                }
+                if (column == 1) {
+                    data[r].T_degC = atof(value);
+                }
+                if (column == 2) {
+                    data[r].PO4uM = atof(value);
+                }
+                if (column == 3) {
+                    data[r].SiO3uM = atof(value);
+                }
+                if (column == 4) {
+                    data[r].NO2uM = atof(value);
+                }
+                if (column == 5) {
+                    data[r].NO3uM = atof(value);
+                }
+                if (column == 6) {
+                    data[r].Salnty = atof(value);
+                }
+
+                if (column == 7) {
+                    data[r].O2ml_L = atof(value);
+                }
+
+                value = strtok(NULL, ", ");
+                column++;
+            }
+
+            printf("\n");
+        }
+
+        fclose(fp);
+}
+}
+//function to remove th "/" from the dates
+void removeChar(char* s, char c)
+{
+
+    int j, n = strlen(s);
+    for (int i = j = 0; i < n; i++)
+        if (s[i] != c)
+            s[j++] = s[i];
+
+    s[j] = '\0';
+}
+
+
+//function to swap the date as YYYY/MM/DD to be in ascending order
+void swapdate (char* str)
+{
+    char temp;
+    int n;
+
+    n = strlen(str);
+	// swap str[0] and str[n-4]
+	temp = str[0];
+	str[0] = str[n - 4];
+	str[n - 4] = temp;
+	// swap str[1] and str[n-3]
+	temp = str[1];
+	str[1] = str[n - 3];
+	str[n - 3] = temp;
+	// swap str[2] and str[n-2]
+	temp = str[2];
+	str[2] = str[n - 2];
+	str[n - 2] = temp;
+	// swap str[3] and str[n-1]
+	temp = str[3];
+	str[3] = str[n - 1];
+	str[n - 1] = temp;
+
+}
+
+
+// An AVL tree node
+struct Node
+{
+    int key;
+    struct Node *left;
+    struct Node *right;
+    int height;
+};
+
+// A utility function to get maximum of two integers
+int max(int a, int b);
+
+// A utility function to get the height of the tree
+int height(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return 1+max(height(N->left), height(N->right));
+}
+
+// A utility function to get maximum of two integers
+int max(int a, int b)
+{
+    return (a > b)? a : b;
+}
+
+/* Helper function that allocates a new node with the given key and
+    NULL left and right pointers. */
+struct Node* newNode(int key)
+{
+    struct Node* node = (struct Node*)
+                        malloc(sizeof(struct Node));
+    node->key   = key;
+    node->left   = NULL;
+    node->right  = NULL;
+    node->height = 0;  // new node is initially added at leaf
+    return(node);
+}
+
+// A utility function to right rotate subtree rooted with y
+// See the diagram given above.
+struct Node *rightRotate(struct Node *y)
+{
+    struct Node *x = y->left;
+    struct Node *T2 = x->right;
+
+    // Perform rotation
+    x->right = y;
+    y->left = T2;
+
+    // Update heights
+    y->height = height(y);
+    x->height = height(x);
+
+    // Return new root
+    return x;
+}
+
+// A utility function to left rotate subtree rooted with x
+// See the diagram given above.
+struct Node *leftRotate(struct Node *x)
+{
+    struct Node *y = x->right;
+    struct Node *T2 = y->left;
+
+    // Perform rotation
+    y->left = x;
+    x->right = T2;
+
+    //  Update heights
+    x->height = height(x);
+    y->height = height(y);
+
+    // Return new root
+    return y;
+}
+
+// Get Balance factor of node N
+int getBalance(struct Node *N)
+{
+    if (N == NULL)
+        return 0;
+    return height(N->left) - height(N->right);
+}
+
+// Recursive function to insert a key in the subtree rooted
+// with node and returns the new root of the subtree.
+struct Node* insert(struct Node* node, int key)
+{
+    /* 1.  Perform the normal BST insertion */
+    if (node == NULL)
+        return(newNode(key));
+
+    if (key < node->key)
+        node->left  = insert(node->left, key);
+    else if (key > node->key)
+        node->right = insert(node->right, key);
+    else // Equal keys are not allowed in BST
+        return node;
+
+    /* 2. Update height of this ancestor node */
+    node->height = height(node);
+
+    /* 3. Get the balance factor of this ancestor
+          node to check whether this node became
+          unbalanced */
+    int balance = getBalance(node);
+
+    // If this node becomes unbalanced, then
+    // there are 4 cases
+
+    // Left Left Case
+    if (balance > 1 && key < node->left->key)
+        return rightRotate(node);
+
+    // Right Right Case
+    if (balance < -1 && key > node->right->key)
+        return leftRotate(node);
+
+    // Left Right Case
+    if (balance > 1 && key > node->left->key)
+    {
+        node->left =  leftRotate(node->left);
+        return rightRotate(node);
+    }
+
+    // Right Left Case
+    if (balance < -1 && key < node->right->key)
+    {
+        node->right = rightRotate(node->right);
+        return leftRotate(node);
+    }
+
+    /* return the (unchanged) node pointer */
+    return node;
+}
+
+// A utility function to print preorder traversal
+// of the tree.
+// The function also prints height of every node
+void preOrder(struct Node *root)
+{
+    if(root != NULL)
+    {
+        printf("%d ", root->key);
+        preOrder(root->left);
+        preOrder(root->right);
+    }
+}
+
+/* Driver program to test above function*/
+int main()
+{
+    data d[1407];
+    openCSV(d); //call the function that read the csv file
+    int s=sizeof(d)/sizeof(d[0]); //used to find right limit, binary search
+
+  struct Node *root = NULL;
+
+  /* Constructing tree given in the above figure */
+  for (int i=0; i<1406; i++) {
+    root = insert(root, d[i].date );
+  }
+
+
+  printf("Preorder traversal of the constructed AVL"
+         " tree is \n");
+  preOrder(root);
+
+  return 0;
+}
